c4fc14fde6b236f35aa9ea81a38bf0b3eab721cd
Laura <lhernan53112@gmail.com>
2020-07-23 16:25:40 -0400

    Added a Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <h2 className="title">Employee Directory</h2>
+            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </nav>
+    );
+}
+
+export default Header;

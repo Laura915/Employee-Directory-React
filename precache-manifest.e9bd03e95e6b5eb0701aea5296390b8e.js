43b826bac173a79031ba0be148cc86d00d758e7a
Laura <lhernan53112@gmail.com>
2020-07-25 09:46:46 -0400

    Updates

---
+++ precache-manifest.e9bd03e95e6b5eb0701aea5296390b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a17437c61997dd9ad532e992f88983",
+    "url": "/Employee-Directory-React/index.html"
+  },
+  {
+    "revision": "1a52368c774acf4c27ef",
+    "url": "/Employee-Directory-React/static/css/main.62a87911.chunk.css"
+  },
+  {
+    "revision": "ef27cbd88b1aaff86419",
+    "url": "/Employee-Directory-React/static/js/2.4d7257b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory-React/static/js/2.4d7257b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a52368c774acf4c27ef",
+    "url": "/Employee-Directory-React/static/js/main.c4e5c1b9.chunk.js"
+  },
+  {
+    "revision": "de9b83b534df7e91f354",
+    "url": "/Employee-Directory-React/static/js/runtime-main.60467e6a.js"
+  }
+]);
